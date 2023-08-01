3ab576166dc26b4a3de51fc8d1a28dfbf3c94102
Tkfrnfl <rul64@naver.com>
2023-08-01 16:38:21 +0900

    init and check test

---
+++ wanted/src/test/java/com/example/wanted/WantedApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
